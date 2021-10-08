fdeb5a7a3a57779feedcdfa03d90c1c6dd859cc5
priyath <priyath@bitsmedia.com>
2021-10-08 18:50:37 +0530

    fixed path

--- packages/root-config/src/bitsmedia-root-config.ts
+++ packages/root-config/src/bitsmedia-root-config.ts
@@ -7,7 +7,7 @@ import {
 
 const routes = constructRoutes(document.querySelector("#single-spa-layout"));
 const applications = constructApplications({
-  // test comment to trigger deploymen
+  // test comment to trigger deployment
   routes,
   loadApp({ name }) {
     return System.import(name);

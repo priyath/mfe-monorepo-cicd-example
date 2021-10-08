464142533bd217666b7abd701543f9ff8c8b0d2d
priyath <priyath@bitsmedia.com>
2021-10-08 12:21:44 +0530

    mfe initial commit

---
+++ packages/app-shell/src/root.component.tsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+import { Link, navigate } from "@reach/router";
+
+export default function Root(props) {
+
+  return (
+    <div className="navbar navbar-dark bg-dark navbar-expand-lg">
+      {(
+        <>
+          <Link className="navbar-brand" to="/">
+            <img
+              src="https://single-spa.js.org/img/logo-white-bgblue.svg"
+              className="d-inline-block align-top"
+              height="30"
+              width="30"
+              alt=""
+            />
+            Microfrontends Demo
+          </Link>
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-link" to="/svod">
+                  SVOD
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/marketplace">
+                  Marketplace
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/moments">
+                  Moments
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div onClick={()=>{}}>
+            <em className="text-white">Logout</em>
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
